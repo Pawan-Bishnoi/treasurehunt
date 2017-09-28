d7aaecc7537aa1f4a2ec340b8885e78eea1eb3cc
Pawan Bishnoi <pawank@wiziq.com>
2017-09-28 23:47:55 +0530

    added prod config

--- common/config.js
+++ common/config.js
@@ -9,4 +9,16 @@ config.db = {
 	url : "mongodb://localhost/hunt"
 };
 
+
+if (process.env.PORT) {
+	/*
+	 * Must be Heroku
+	 * configure accordingly !
+	 */
+
+	config.app.port = process.env.PORT;
+	config.app.env = 'prod';
+	config.db.url = 'mongodb://not_a_robot:Computer1@cluster0-shard-00-00-k9p7y.mongodb.net:27017,cluster0-shard-00-01-k9p7y.mongodb.net:27017,cluster0-shard-00-02-k9p7y.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin';
+}
+
 module.exports = config;
